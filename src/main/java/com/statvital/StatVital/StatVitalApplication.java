ef3489bbe2e5bd4e36d225f6501ddefb7fdff23c
Oluwaseyi69 <oluwaseyitemitope69@gmail.com>
2024-02-27 10:14:11 +0100

    Initial Commit

---
+++ src/main/java/com/statvital/StatVital/StatVitalApplication.java
@@ -0,0 +1,13 @@
+package com.statvital.StatVital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatVitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatVitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/statvital/StatVital/StatVitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statvital.StatVital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatVitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
